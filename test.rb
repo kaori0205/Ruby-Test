3160032c59540ea268d946c1a0bff13d950f15f0
kaori0205 <320223483+kaori0205@users.noreply.github.com>
2026-09-06 11:34:21 +0900

    [update]オブジェクト

--- test.rb
+++ test.rb
@@ -1,3 +1,29 @@
-puts "田中さんの年齢は" + 28.to_s + "です"
-puts 10 + "90" .to_i
-puts 10 + 90
+def say_hello(name)
+  puts "hello, #{name}!"
+end
+
+say_hello("岡田")
+
+
+
+def say_hello(name)
+  "hello, #{name}!"
+end
+
+puts say_hello("岡田")
+
+
+
+def say_hello(name)
+    return "hello, #{name}!"
+    "thank you, #{name}!"
+end
+
+puts say_hello("岡田")
+
+puts "ruby" .length
+puts "ruby" .reverse
+puts "Rubyの基礎学習" .include?("Ruby")
+
+num = [1, 2, 2, 3, 4, 5, 5]
+puts num.uniq
